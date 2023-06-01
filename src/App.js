a3ebc52e00fc78307fd63ae3df020296df0a31c2
hieubd <buiduchieuuet16@gmail.com>
2023-06-01 13:25:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import BasicLayout from "./layout/Layout";
+import "./App.css";
 
 const App = () => {
     return (
--- src/components/ImageRegconition/index.jsx
+++ src/components/ImageRegconition/index.jsx
@@ -1,26 +1,48 @@
 import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import { Button, Col, Image, Row, Upload } from "antd";
-import { useState } from "react";
+import { Button, Col, Image, Row, Upload, message } from "antd";
+import { useRef, useState } from "react";
 
 const ImageRecognition = () => {
+    const wrapRef = useRef();
+
     const [imageSrc, setImageSrc] = useState("");
     const [prediction, setPrediction] = useState([]);
+    const [loadingPredict, setLoadingPredict] = useState(false);
+    const [widthWrap, setWidthWrap] = useState(0);
+    const [imageWidth, setImageWidth] = useState(null);
 
     const propsUpload = {
         name: 'file',
         action: '',
+        // onChange(info) {
+        //     if (info.file.status !== 'uploading') {
+        //         // setImageSrc(info.file.originFileObj);
+        //         const imgElement = document.createElement("img");
+        //         imgElement.src = URL.createObjectURL(info.file.originFileObj);
+
+        //         imgElement.onload = async () => {
+        //             await detectImageObject(imgElement);
+        //         }
+
+        //     }
+        //     if (info.file.status === 'done') {
+        //         console.log(info.file, info.fileList);
+        //     }
+        // },
         onChange(info) {
             if (info.file.status !== 'uploading') {
-                setImageSrc(info.file.originFileObj);
-                const imgElement = document.createElement("img");
+                const imgElement = document.createElement('img');
                 imgElement.src = URL.createObjectURL(info.file.originFileObj);
 
                 imgElement.onload = async () => {
+                    setImageWidth(imgElement.width);
+                    setImageSrc(info.file.originFileObj);
                     await detectImageObject(imgElement);
-                }
+                };
             }
+
             if (info.file.status === 'done') {
                 console.log(info.file, info.fileList);
             }
@@ -28,10 +50,15 @@ const ImageRecognition = () => {
     };
 
     const detectImageObject = async (image) => {
+        setLoadingPredict(true);
+        setWidthWrap(wrapRef.current.offsetWidth);
+        console.log(wrapRef.current.offsetWidth);
         const model = await cocoSsd.load({});
         const predictions = await model.detect(image, 10);
 
         console.log(predictions);
+        setPrediction(predictions);
+        setLoadingPredict(false);
     }
 
     return (
@@ -40,19 +67,45 @@ const ImageRecognition = () => {
                 <div style={{
                     border: '1px solid rgb(215 214 214)',
                     borderRadius: 8,
-                    textAlign: 'center'
-                }}>
+                    textAlign: 'center',
+                    position: 'relative',
+                    margin: 'auto',
+                }}
+                    className="image-recognize-box"
+                    ref={wrapRef}
+                >
                     {imageSrc &&
-                        <Image
+                        <img
                             src={URL.createObjectURL(imageSrc)}
-                            preview={false}
                         />
                     }
+                    {prediction.map((item, index) => {
+                        return (
+                            <div
+                                style={{
+                                    position: 'absolute',
+                                    left: (widthWrap / 2 - imageWidth / 2) + item.bbox[0],
+                                    top: item.bbox[1],
+                                    width: item.bbox[2],
+                                    height: item.bbox[3],
+                                    border: '2px solid green'
+                                }}
+                                key={index}
+                                className="recognize-box"
+                                datacustomattribute={item.class + " " + (item.score * 100).toFixed(2) + "%"}
+                            ></div>
+                        );
+                    })}
                 </div>
             </Col>
-            <Col span={24} style={{ marginTop: 16 }}>
-                <Upload {...propsUpload} accept="image/*" maxCount={1} onPreview={false}>
-                    <Button type="primary">Upload Image</Button>
+            <Col span={24} style={{ marginTop: 16, textAlign: 'center' }}>
+                <Upload
+                    {...propsUpload}
+                    accept="image/*"
+                    maxCount={1}
+                    showUploadList={false}
+                >
+                    <Button type="primary" loading={loadingPredict}>{loadingPredict ? "Recognizing..." : "Upload Image"}</Button>
                 </Upload>
             </Col>
         </Row>
